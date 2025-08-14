b61455f6384eec4e5ff275cb74dcf7aa1d243b5f
Kadhir Manickam <kadhirvelm@gmail.com>
2025-08-14 15:17:14 -0700

    Fix build

--- packages/backend/src/database/prisma.service.ts
+++ packages/backend/src/database/prisma.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@tsm-example/database";
 import type { ConfigurationService } from "@/configuration/configuration.service";
 
@@ -12,16 +11,6 @@ export class PrismaService {
 	constructor(private configService: ConfigurationService) {
 		const environment = this.configService.get("NODE_ENV");
 		this.logger.log(`Creating prisma client - ${environment}`);
-
-		if (environment === "production") {
-			const adapter = new PrismaNeon({
-				connectionString: this.configService.get("DATABASE_HOST_URL"),
-			});
-
-			// biome-ignore lint/suspicious/noExplicitAny: something is wrong with the types here
-			this.client = new PrismaClient({ adapter: adapter as any });
-		} else {
-			this.client = new PrismaClient();
-		}
+		this.client = new PrismaClient();
 	}
 }
